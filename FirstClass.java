4166ea4fc1fb410a91c8dfbbd6e52e372794373f
Shantanu Prasad <prasad.shantanu2020@gmail.com>
2025-07-29 11:33:37 +0530

    Hello world

---
+++ FirstClass.java
@@ -0,0 +1,7 @@
+class FirstClass {
+    public static void main (String args[]){
+        System.out.println("Hello World");
+        System.out.println("My self Shantanu");
+        System.out.println(("Its a good day "));
+    }
+}
